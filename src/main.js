0fec0ff3f0037009bb2c764cf967c23d3b839bc6
Marta <martafontb@gmail.com>
2021-06-16 17:02:16 +0200

    first commit

--- src/main.js
+++ src/main.js
@@ -1,7 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import BootstrapVue from 'bootstrap-vue'
+Vue.use(BootstrapVue);
+
+// Import Bootstrap an BootstrapVue CSS files (order is important)
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
 Vue.config.productionTip = false
+
+
 
 new Vue({
   render: h => h(App),
